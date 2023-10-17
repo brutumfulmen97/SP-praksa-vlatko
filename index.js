4488e67aa5442cf19dc0a53188361d4169c94099
brutumfulmen97 <109813663+brutumfulmen97@users.noreply.github.com>
2023-10-17 11:41:59 +0200

    responsive design

---
+++ index.js
@@ -0,0 +1,16 @@
+// const navXl = document.querySelector(".header__nav-xl");
+// const navMd = document.querySelector(".header__nav-md");
+
+// console.log(navMd, navXl);
+
+// window.addEventListener("resize", () => {
+//     if (window.innerWidth < 1320) {
+//         navXl.classList.add("hidden");
+//         console.log(123);
+//         navMd.classList.remove("hidden");
+//     } else {
+//         navXl.classList.remove("hidden");
+//         console.log(312);
+//         navMd.classList.add("hidden");
+//     }
+// });

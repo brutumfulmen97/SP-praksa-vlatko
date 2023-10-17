5bcfef80986bef948bbbb90ede9d8e9bfc335f72
brutumfulmen97 <109813663+brutumfulmen97@users.noreply.github.com>
2023-10-17 10:27:59 +0200

    media query ne radi?????????????

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,6 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 
-// const buildStyles = function () {
-//     return gulp
-//         .src("./css/**/*.scss")
-//         .pipe(sass.sync().on("error", sass.logError))
-//         .pipe(gulp.dest("./test"));
-// };
-
-// const watch = function () {
-//     gulp.watch("./css/**/*.scss", ["sass"]);
-// };
-
-// module.exports = {
-//     default: gulp.parallel(buildStyles, watch),
-//     build: buildStyles,
-//     watch: watch,
-// };
-
 gulp.task("watch", function () {
     // Define the files to watch
     gulp.watch("./css/**/*.scss", gulp.series("sass"));

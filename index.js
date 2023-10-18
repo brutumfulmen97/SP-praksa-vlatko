d2250aab0565afa64040874249663d6b08bdf77b
brutumfulmen97 <109813663+brutumfulmen97@users.noreply.github.com>
2023-10-18 09:38:12 +0200

    swiper start

--- index.js
+++ index.js
@@ -11,3 +11,25 @@ closeBtn.addEventListener("click", () => {
         mobileMenu.classList.remove("mobile-menu-animation");
     }, 0);
 });
+
+const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    direction: "vertical",
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: ".swiper-pagination",
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: ".swiper-scrollbar",
+    },
+});

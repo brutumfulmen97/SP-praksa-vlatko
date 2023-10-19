71511daa8d62bf1c7a4a0c1ad0081068661cb287
brutumfulmen97 <109813663+brutumfulmen97@users.noreply.github.com>
2023-10-19 09:54:27 +0200

    nav i landing nov stil

--- index.js
+++ index.js
@@ -14,7 +14,24 @@ closeBtn.addEventListener("click", () => {
     }, 0);
 });
 
-const swiper = new Swiper(".swiper", {
+const swiper1 = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    // pagination: {
+    //     el: ".swiper-pagination",
+    //     clickable: true,
+    // },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+const swiper2 = new Swiper(".swiper", {
     effect: "coverflow",
     grabCursor: true,
     centeredSlides: true,
